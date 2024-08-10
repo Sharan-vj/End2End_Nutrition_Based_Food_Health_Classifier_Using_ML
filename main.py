af7aaecd4e5724b965e41134b6a8f908cf7b3ab5
Sharan-vj <sharanvj678@gmail.com>
2024-08-10 12:21:51 +0530

    fix: code update

--- main.py
+++ main.py
@@ -5,52 +5,48 @@ from foodhealthclassifier.pipeline.stage_03_data_transformation import DataTrans
 from foodhealthclassifier.pipeline.stage_04_model_trainer import ModelTrainerPipeline
 
 STAGE_NAME = 'Data Ingestion'
-if __name__ == "__main__":
-    try: 
-        logger.info(f">>>>> Stage {STAGE_NAME} Started <<<<<")
-        obj = DataIngestionPipeline()
-        obj.main()
-        logger.info(f">>>>> Stage {STAGE_NAME} Completed <<<<<")
-    
-    except Exception as e:
-        logger.log(level=1, msg=e)
-        raise e
+try: 
+    logger.info(f">>>>> Stage {STAGE_NAME} Started <<<<<")
+    obj = DataIngestionPipeline()
+    obj.main()
+    logger.info(f">>>>> Stage {STAGE_NAME} Completed <<<<<")
+
+except Exception as e:
+    logger.log(level=1, msg=e)
+    raise e
     
 
 STAGE_NAME = 'Data Validation'
-if __name__ == '__main__':
-    try:
-        logger.info(f'>>>>> Stage {STAGE_NAME} Started <<<<<')
-        obj = DataValidationPipeline()
-        obj.main()
-        logger.info(f'>>>>> Stage {STAGE_NAME} Completed <<<<<')
-    
-    except Exception as e:
-        logger.log(msg=e, level=1)
-        raise(e)
+try:
+    logger.info(f'>>>>> Stage {STAGE_NAME} Started <<<<<')
+    obj = DataValidationPipeline()
+    obj.main()
+    logger.info(f'>>>>> Stage {STAGE_NAME} Completed <<<<<')
+
+except Exception as e:
+    logger.log(msg=e, level=1)
+    raise(e)
     
 
 STAGE_NAME = 'Data Transformation'
-if __name__ == '__main__':
-    try:
-        logger.info(f'>>>>> Stage {STAGE_NAME} Started <<<<<')
-        obj = DataTransformationPipeline()
-        obj.main()
-        logger.info(f'>>>>> Stage {STAGE_NAME} Completed <<<<<')
+try:
+    logger.info(f'>>>>> Stage {STAGE_NAME} Started <<<<<')
+    obj = DataTransformationPipeline()
+    obj.main()
+    logger.info(f'>>>>> Stage {STAGE_NAME} Completed <<<<<')
 
-    except Exception as e:
-        logger.log(msg=e, level=1)
-        raise(e)
+except Exception as e:
+    logger.log(msg=e, level=1)
+    raise(e)
     
 
 STAGE_NAME = 'Model Training'
-if __name__ == '__main__':
-    try:
-        logger.info(f'>>>>> Stage {STAGE_NAME} Started <<<<<')
-        obj = ModelTrainerPipeline()
-        obj.main()
-        logger.info(f'>>>>> Stage {STAGE_NAME} Completed <<<<<')
+try:
+    logger.info(f'>>>>> Stage {STAGE_NAME} Started <<<<<')
+    obj = ModelTrainerPipeline()
+    obj.main()
+    logger.info(f'>>>>> Stage {STAGE_NAME} Completed <<<<<')
 
-    except Exception as e:
-        logger.log(msg=e, level=1)
-        raise(e)
+except Exception as e:
+    logger.log(msg=e, level=1)
+    raise(e)
